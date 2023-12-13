65f2695fb55ba3c54b651abbf8d5a5161ff08f6f
Markicola12 <antalm481@gmail.com>
2023-12-13 10:27:45 +0100

    comment írása a szam valtozóhoz

--- rulett.py
+++ rulett.py
@@ -1,9 +1,9 @@
-import random
+import random  
 import os
 
 tipp = int(input("Tippelj egy egész számot 1 és 6 között: "))
 
-szam = random.randint(1,6)
+szam = random.randint(1,6) # generál egy random számot
 
 if tipp == szam:
     print("Nyertél!")
